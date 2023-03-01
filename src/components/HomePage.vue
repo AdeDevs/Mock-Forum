4aafc08f32af569c843532b5ae263c7af9bcbd97
AdeDevs <adeyemiakinyemi01@gmail.com>
2023-03-01 23:56:40 +0100

    Updated

---
+++ src/components/HomePage.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>Home Page</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Facebook Home Page",
+    props: {
+        msg: String
+    }
+}
+</script>
